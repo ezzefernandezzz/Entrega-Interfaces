ccb8f95ae992cd9261ce42415dae9a5e1830e555
Lautaro Morales <lautamorales17@gmail.com>
2022-10-14 15:40:50 -0300

    Se crea overlay automatico (por ahora todos los juegos redirigen a pag 4 en linea.

---
+++ Entrega2/js/home-card-hover.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    let gamecards = document.getElementsByClassName('gamecard-wide');
+    crearOverlays(gamecards);
+    let gamecards2 = document.getElementsByClassName('gamecard-small');
+    crearOverlays(gamecards2);
+    let gamecards3 = document.getElementsByClassName('gamecard-big');
+    crearOverlays(gamecards3);
+
+    function crearOverlays(gamecards){
+        for(gamecard of gamecards) {
+            let alt = gamecard.firstElementChild.alt;
+            console.log(alt);
+            console.log(gamecard.children.length);
+            let price = gamecard.firstElementChild.dataset.price;
+            let card_overlay = ``;
+            if (price === undefined) {
+                card_overlay = 
+                    `<div class="overlay">
+                        <text>${alt}</text>
+                        <a href="4enlinea.html"><button class="btn-jugar">Jugar</button></a>
+                    </div> `
+            } else {
+                card_overlay = 
+                    `<div class="overlay">
+                        <text>${alt}</text>
+                        <text>${price}</text>
+                        <button class="btn-jugar">Comprar</button>
+                    </div> `;
+            }
+
+            gamecard.insertAdjacentHTML('beforeend', card_overlay);
+        }
+    }
+
+
+});
