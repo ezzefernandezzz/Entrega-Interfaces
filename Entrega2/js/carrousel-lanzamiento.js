ecf557fee76cb3ed7221193d6011bcb71b532e24
Ezequiel Fernandez <ezzefernandezzz@gmail.com>
2022-10-14 02:52:33 -0300

    se agrega carrousel lanzamiento y pantalla de carga home

---
+++ Entrega2/js/carrousel-lanzamiento.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const prev = document.querySelector('.prev')
+    const next = document.querySelector('.next')
+    const slider = document.querySelector('.slider')
+
+    prev.addEventListener('click', () => {
+        slider.scrollLeft -= 300
+    })
+
+    next.addEventListener('click', () => {
+        slider.scrollLeft += 300
+    })
+})
---
+++ Entrega2/js/loader.js
@@ -0,0 +1,12 @@
+const ventana_carga = document.querySelector('.prueba')
+
+CSS.registerProperty({
+    name: "--p",
+    syntax: "<integer>",
+    initialValue: 0,
+    inherits: true,
+  });
+
+setTimeout(function(){
+    ventana_carga.style.visibility = "hidden";
+}, 5000);
