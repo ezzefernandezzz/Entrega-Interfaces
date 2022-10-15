7f5fb5a9fc6df3403563b837995855a0f178c75e
Lautaro Morales <lautamorales17@gmail.com>
2022-10-15 11:06:42 -0300

    Se agrega comportamiento de estrellas para puntuar en pag 4 en linea

---
+++ Entrega2/js/puntuar.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', () => {
+    
+    estrellas = document.getElementsByClassName('estrella-puntuar');
+    estrella_hover_url = "../img/iconos/estrella-hover.png";
+    estrella_normal_url = "../img/iconos/estrella-normal.png";
+    estrella_active_url = "../img/iconos/estrella-active.png";
+
+    console.log(estrellas);
+    for(let i = 0; i < estrellas.length; i++) {
+        estrellas[i].addEventListener('mouseover', () => {
+            if (!estrellas[i].classList.contains('puntuado'))
+            for(let j = 0; j <= i; j++) {
+                estrellas[j].src = estrella_hover_url;
+            }
+        });
+        estrellas[i].addEventListener('mouseout', () => {
+            if (!estrellas[i].classList.contains('puntuado'))
+                estrellas[i].src = estrella_normal_url;
+        })
+        estrellas[i].addEventListener('mousedown', () => {
+            for(let j = 0; j <= i; j++) {
+                estrellas[j].src = estrella_active_url;
+                estrellas[j].classList.add('puntuado');
+            }
+        });
+    }
+
+});
