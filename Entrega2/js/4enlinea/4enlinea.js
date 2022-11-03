84c49572d012199963754bf8dcc338984282852c
Ezequiel Fernandez <ezzefernandezzz@gmail.com>
2022-11-03 11:37:11 -0300

    se intenta generar tablero

--- Entrega2/js/4enlinea/4enlinea.js
+++ Entrega2/js/4enlinea/4enlinea.js
@@ -1,8 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => { 
 
-    let tablero = new Tablero(7, 6, 4);
-    let jugador1 = new Jugador("Alfonso", "../img/iconos/perfil.png", (tablero.ancho * tablero.alto) / 2);
-    let jugador2 = new Jugador("Carlos", "../img/iconos/moneda.jpg", (tablero.ancho * tablero.alto) / 2);
+    let canvas = document.getElementById("myCanvas");
+    console.log(canvas);
+    let ctx = canvas.getContext("2d");
+
+    //instancia tablero
+//    let tablero = new Tablero(7, 6, 4); 
+    let tablero = new Tablero(ctx, canvas.offsetWidth, canvas.offsetHeight, 4); 
+    //fichasTotales
+    let cantFichas = (tablero.ancho * tablero.alto);
+    //instancia jugadores
+    let jugador1 = new Jugador("Alfonso", "../img/iconos/perfil.png", cantFichas / 2);
+    let jugador2 = new Jugador("Carlos", "../img/iconos/moneda.jpg", cantFichas / 2);
     tablero.jugadores.push(jugador1);
     tablero.jugadores.push(jugador2);
     tablero.jugadorActual = tablero.jugadores[0];
@@ -25,6 +34,5 @@ window.addEventListener('DOMContentLoaded', () => {
     tablero.tablero[0][5].ficha = fichaTest;
     console.log(tablero.tablero[0][5].ficha);
     */
-    
     
 });
