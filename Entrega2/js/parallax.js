d1636b312a75f559f4e569209cf91dc7bd0e51cd
Lautaro Morales <lautamorales17@gmail.com>
2022-11-20 13:30:08 -0300

    Se agrega suavizado al entrar a la imagen con parallax. Se sube la posicion del sol, y se agrega codigo para resetear los elementos a su posicion original

--- Entrega2/js/parallax.js
+++ Entrega2/js/parallax.js
@@ -1,23 +1,42 @@
 let parallax = document.getElementById('parallax-container');
 let imagenes = parallax.getElementsByTagName('img');
 
+parallax.addEventListener('mouseenter', smoothEnter);
 parallax.addEventListener('mousemove', mouseMove);
 parallax.addEventListener('mouseleave', resetImgPositions);
+
+let posYOriginales = [];
+
+function smoothEnter() {
+    for(let img of imagenes) {
+        img.style.setProperty('transition', 'all 0.2s linear');
+        let compStyles = getComputedStyle(img);
+        posYOriginales.push(parseInt(compStyles.top.replace("px", '')));
+    }
+
+}
 
 function mouseMove(event) {
     let x = event.clientX;
     let y = event.layerY;
     for(let i = 0; i < imagenes.length; i++) {
+        imagenes[i].style.setProperty('transition', 'all 0.1s linear');
         let offsetX =  ((i + 1) * x) / 150;
         let offsetY = Math.abs((i * y) / 250);
+        //imagenes[i].style.setProperty('left', offsetX + "px");
         imagenes[i].style.left = offsetX + "px";
-        imagenes[i].style.top = (offsetY) + "px";
+        //imagenes[i].style.setProperty('top', offsetY + "px");
+        console.log(offsetY + posYOriginales[i] + "px");
+        imagenes[i].style.top =  (offsetY + posYOriginales[i]) + "px";
     }
 }
 
-function resetImgPositions(event) {
-    for (let img of imagenes) {
-        img.style.setProperty('left', 0 + "px");
-        img.style.setProperty('top', 0 + "px");
+function resetImgPositions() {
+    for (let i = 0; i < imagenes.length; i++) {
+        imagenes[i].style.setProperty('transition', 'all 1s linear');
+        //imagenes[i].style.setProperty('left', 0 + "px");
+        //imagenes[i].style.setProperty('top', posYOriginales[i] + "px");
+        imagenes[i].style.left = "0px";
+        imagenes[i].style.top = posYOriginales[i] + "px";
     }
 }
