506e5641c195f9e7a4b0765c6081127819fe8771
Lautaro Morales <lautamorales17@gmail.com>
2022-11-20 11:32:09 -0300

    Se agregan correcciones a codigo parallax, se corrije overflow

--- Entrega2/js/parallax.js
+++ Entrega2/js/parallax.js
@@ -7,20 +7,17 @@ parallax.addEventListener('mouseleave', resetImgPositions);
 function mouseMove(event) {
     let x = event.clientX;
     let y = event.layerY;
-    let baseY = 263;
-    console.log(baseY);
-    for(let i = 1; i < imagenes.length; i++) {
-        let offsetX =  (i * x) / 150;
+    for(let i = 0; i < imagenes.length; i++) {
+        let offsetX =  ((i + 1) * x) / 150;
         let offsetY = Math.abs((i * y) / 250);
-        console.log(offsetY);
         imagenes[i].style.left = offsetX + "px";
-        imagenes[i].style.top = (baseY + offsetY) + "px";
+        imagenes[i].style.top = (offsetY) + "px";
     }
 }
 
 function resetImgPositions(event) {
     for (let img of imagenes) {
-        img.style.setProperty('left', 0);
-        img.style.setProperty('top', 263 + "px");
+        img.style.setProperty('left', 0 + "px");
+        img.style.setProperty('top', 0 + "px");
     }
 }
