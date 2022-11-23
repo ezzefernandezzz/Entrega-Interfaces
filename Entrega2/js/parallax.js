670af3237abc33c2ff6836c1d0ce8fe7f6876966
Lautaro Morales <lautamorales17@gmail.com>
2022-11-23 17:14:34 -0300

    Se agrega inferno trooper a pantalla parallax

--- Entrega2/js/parallax.js
+++ Entrega2/js/parallax.js
@@ -5,12 +5,15 @@ parallax.addEventListener('mouseenter', smoothEnter);
 parallax.addEventListener('mousemove', mouseMove);
 parallax.addEventListener('mouseleave', resetImgPositions);
 
+let posXOriginales = [];
 let posYOriginales = [];
 
 function smoothEnter() {
     for(let img of imagenes) {
+        //img.style.setProperty('transition', 'left 0.2s linear, top 0.2s linear');
         img.style.setProperty('transition', 'all 0.2s linear');
         let compStyles = getComputedStyle(img);
+        posXOriginales.push(parseInt(compStyles.left.replace("px", '')));
         posYOriginales.push(parseInt(compStyles.top.replace("px", '')));
     }
 
@@ -20,23 +23,36 @@ function mouseMove(event) {
     let x = event.clientX;
     let y = event.layerY;
     for(let i = 0; i < imagenes.length; i++) {
+        let offsetX;
+        let offsetY;
+        //imagenes[i].style.setProperty('transition', 'left 0.1s linear, top 0.1s linear');
         imagenes[i].style.setProperty('transition', 'all 0.1s linear');
-        let offsetX =  ((i + 1) * x) / 150;
-        let offsetY = Math.abs((i * y) / 250);
-        //imagenes[i].style.setProperty('left', offsetX + "px");
-        imagenes[i].style.left = offsetX + "px";
-        //imagenes[i].style.setProperty('top', offsetY + "px");
-        console.log(offsetY + posYOriginales[i] + "px");
-        imagenes[i].style.top =  (offsetY + posYOriginales[i]) + "px";
+        if (i == (imagenes.length - 1)) {
+            offsetX = Math.log(i) * x / 50;
+            offsetY = Math.log(i) * y / 75;
+            imagenes[i].style.left = (offsetX + posXOriginales[i - 1]) + "px";
+            imagenes[i].style.top =  (offsetY + posYOriginales[i - 1]) + "px";
+        } else {
+            offsetX = Math.log(i + 1) * x / 50;
+            offsetY = Math.log(i + 1) * y / 75;
+            imagenes[i].style.left = (offsetX + posXOriginales[i]) + "px";
+            imagenes[i].style.top =  (offsetY + posYOriginales[i]) + "px";
+        }
     }
 }
 
 function resetImgPositions() {
     for (let i = 0; i < imagenes.length; i++) {
+        //imagenes[i].style.setProperty('transition', 'left 1s linear, top 1s linear');
         imagenes[i].style.setProperty('transition', 'all 1s linear');
         //imagenes[i].style.setProperty('left', 0 + "px");
         //imagenes[i].style.setProperty('top', posYOriginales[i] + "px");
-        imagenes[i].style.left = "0px";
-        imagenes[i].style.top = posYOriginales[i] + "px";
+        if (i == (imagenes.length - 1)) {
+            imagenes[i].style.left = posXOriginales[i - 1] + "px";
+            imagenes[i].style.top = posYOriginales[i - 1] + "px";    
+        } else {
+            imagenes[i].style.left = posXOriginales[i] + "px";
+            imagenes[i].style.top = posYOriginales[i] + "px";
+        }
     }
 }
