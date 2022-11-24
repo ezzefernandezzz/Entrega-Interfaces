95baf75f7c7c64e298af3725f75926d90a320d6e
Lautaro Morales <lautamorales17@gmail.com>
2022-11-23 21:01:17 -0300

    Se agrega animacion a carrouseles del home.

--- Entrega2/js/carrousel.js
+++ Entrega2/js/carrousel.js
@@ -1,44 +1,95 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     /* Carrousel's */
-    const prev = document.querySelector('.prev')
-    const next = document.querySelector('.next')
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
+    
+    const prev2 = document.querySelector('.prev2');
+    const next2 = document.querySelector('.next2');
+    const slider = document.querySelector('.slider');
+    const slider2 = document.querySelector('.slider2');
+    
+    let interval_s1;
+    let interval_s2;
 
-    const prev2 = document.querySelector('.prev2')
-    const next2 = document.querySelector('.next2')
+    slider.addEventListener('mouseleave', () => {
+        interval_s1 = startAnimation(slider);
+    });
+    slider.addEventListener('mouseenter', () => {
+        stopAnimation(interval_s1);
+    });
 
-    const slider = document.querySelector('.slider')
-    const slider2 = document.querySelector('.slider2')
-    /* Carrousel's */
-
-    /* let nombreJuego = document.querySelector(".gamecard-big img");
-    console.log(nombreJuego.alt)
-
-    nombreJuego.addEventListener('mouseover', () => {
-        let game_card = document.querySelector(".gamecard-big")
-        let overlay = document.querySelector(".overlay")
-
-        // game_card.classList.add("overlay");
-        overlay.style.width = game_card.clientWidth;
+    slider2.addEventListener('mouseleave', () => {
+        interval_s2 = startAnimation(slider2);
+    });
+    slider2.addEventListener('mouseenter', () => {
+        stopAnimation(interval_s2);
+    });
 
 
-    }) */
+    interval_s1 = startAnimation(slider);
+    interval_s2 = startAnimation(slider2);
+
+    function startAnimation(element, interval) {
+        let cards = element.getElementsByClassName('gamecard');
+        console.log(cards);
+        return interval = setInterval(() => {
+            //element.style.setProperty("animation", "carousel-bounce 0.7s forwards ease-in-out");
+            for (let card of cards) {
+                card.style.setProperty("animation", "carousel-bounce 0.7s forwards ease-in-out");
+            }
+            setTimeout(() => {
+                for (let card of cards) {
+                    card.style.removeProperty("animation");
+                }
+                //element.style.removeProperty("animation");
+            }, 700);
+            element.scrollLeft += 400;
+            if (element.scrollLeft == element.scrollWidth - element.clientWidth) {
+                element.scrollLeft = 0;
+            };
+        }, 4000);
+    };
+
+    function stopAnimation(interval_id) {
+        clearInterval(interval_id);
+    };
 
     prev.addEventListener('click', () => {
-        slider.scrollLeft -= 300
-    })
+        if (slider.scrollLeft == 0) {
+            playShakeAnimation(slider);
+        }
+        else
+            slider.scrollLeft -= 400
+    });
 
     next.addEventListener('click', () => {
-        slider.scrollLeft += 300
-    })
-
+        if (slider.scrollLeft == slider.scrollWidth - slider.clientWidth) {
+            playShakeAnimation(slider);
+        }
+        else
+            slider.scrollLeft += 400
+    });
 
     prev2.addEventListener('click', () => {
-        slider2.scrollLeft -= 300
-    })
+        if (slider2.scrollLeft == 0) {
+            playShakeAnimation(slider2);
+        } else
+            slider2.scrollLeft -= 400
+    });
 
     next2.addEventListener('click', () => {
-        slider2.scrollLeft += 300
-    })
+        if (slider2.scrollLeft == slider2.scrollWidth - slider2.clientWidth) {
+            playShakeAnimation(slider2);
+        } else 
+            slider2.scrollLeft += 400
+    });
+
+    function playShakeAnimation(element) {
+        element.style.setProperty("animation", "carousel-shake 0.5s forwards ease-in");
+        setTimeout(() => {
+            element.style.removeProperty("animation");
+        }, 500);
+    }
 
 })
