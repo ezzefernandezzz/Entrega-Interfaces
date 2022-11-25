585a2693d407bb61726e3f402cbdf0e2faddcd21
Lautaro Morales <lautamorales17@gmail.com>
2022-11-24 23:05:23 -0300

    Se modifica efecto on scroll

--- Entrega2/js/on-scroll.js
+++ Entrega2/js/on-scroll.js
@@ -1,18 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
     let sections = document.getElementsByClassName('section');
     let container = document.querySelector('.on-scroll-content');
-
     container.addEventListener('wheel', isBeingScrolled);
+    //body.addEventListener('scroll', isBeingScrolled);
 
     function isBeingScrolled(event) {
         let y = event.pageY;
         for (let section of sections) {
-            isBeingSeen(y, section);
+            isOnScreen(y, section);
         }
 
     }
 
-    function isBeingSeen(y, section) {
+    function isOnScreen(y, section) {
         if (y > section.offsetTop && y < section.clientHeight + section.offsetTop) {
             section.style.setProperty("opacity", 1);
         }
