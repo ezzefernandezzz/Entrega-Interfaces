1b4d020b9d146fbef863d52f0e890c8172c7fab8
Lautaro Morales <lautamorales17@gmail.com>
2022-11-26 20:32:41 -0300

    Se combina imagenes con posicion fija con scrolleo de texto, falta reducir velocidad de scroll para una mejor lectura del texto

--- Entrega2/js/on-scroll-2.js
+++ Entrega2/js/on-scroll-2.js
@@ -1,15 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
+
     let cont_texto = document.querySelector('.container-historia');
     let div_texto = cont_texto.querySelector('.texto-historia');
     let og_div_offset = div_texto.offsetTop;
-    let compStyle_div = getComputedStyle(div_texto);
     let compStyle_h2 = getComputedStyle(cont_texto.querySelector('h2'));
     let compStyle_p = getComputedStyle(cont_texto.querySelector('p'));
     let full_text_height = parseInt(compStyle_h2.height.replace("px", '')) + parseInt(compStyle_p.height.replace("px", ''));
-    cont_texto.style.setProperty('height', full_text_height + "px");
-    document.addEventListener('scroll', isTextoVisibible);
+    cont_texto.style.height = full_text_height + "px";
 
-    function isTextoVisibible(event) {
+    let imagenes = cont_texto.getElementsByTagName('img');
+
+    document.addEventListener('scroll', scrollText);
+
+    function scrollText(event) {
         let posScroll = document.documentElement.scrollTop;
         if (posScroll >= cont_texto.offsetTop && posScroll <= cont_texto.clientHeight + cont_texto.offsetTop) {
             let posScroll_in_section = posScroll - og_div_offset;
@@ -17,12 +20,35 @@ window.addEventListener('DOMContentLoaded', () => {
                 let pos_percentage = (posScroll - div_texto.offsetTop) / 10;
                 div_texto.style.setProperty('position', 'sticky');
                 div_texto.style.setProperty('top', pos_percentage + "%");
-                let cont_texto_percentage = cont_texto.clientHeight / 100;
-                console.log((posScroll_in_section) / cont_texto_percentage);
+                let cont_texto_percentage_px = cont_texto.clientHeight / 100;
                 let text_percentage_px = (div_texto.scrollHeight / 100);
-                let scroll_percentage = (posScroll_in_section / cont_texto_percentage) * text_percentage_px;
-                div_texto.scrollTo(0, scroll_percentage);
+                let scroll_percentage = posScroll_in_section / cont_texto_percentage_px;
+                let text_scroll_px = scroll_percentage * text_percentage_px;
+                let visible_img_number;
+                if (scroll_percentage < 33.3) {
+                    visible_img_number = 0;
+                } else if (scroll_percentage < 66.6) {
+                    visible_img_number = 1;
+                } else if (scroll_percentage <= 100) {
+                    visible_img_number = 2;
+                }
+                hideAllImagesExcept(visible_img_number);
+                div_texto.scrollTo(0, text_scroll_px);
             }
+        } else
+            hideAllImages();
+    }
+
+    function hideAllImagesExcept(index) {
+        for (let img of imagenes) {
+            img.style.setProperty("opacity", 0);
+        }
+        imagenes[index].style.setProperty("opacity", 1);
+    }
+
+    function hideAllImages() {
+        for (let img of imagenes) {
+            img.style.setProperty("opacity", 0);
         }
     }
 
--- Entrega2/js/on-scroll.js
+++ Entrega2/js/on-scroll.js
@@ -1,16 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     
     let sections = document.getElementsByClassName('section');
-    /* let container = document.querySelector('.on-scroll-content'); */
-    /* container.addEventListener('wheel', isBeingScrolled); */
-
     document.addEventListener("scroll", isBeingScrolled);
 
-    isBeingScrolled(); //para cargar la primer img sin scrollear y que no aparezcan todas juntas
+    isBeingScrolled();
 
     function isBeingScrolled(event) {
-        /* let y = event.pageY; */
-        console.log(document);
         let y = document.documentElement.scrollTop + 150; 
         
         for (let section of sections) {
@@ -21,8 +16,6 @@ window.addEventListener('DOMContentLoaded', () => {
     function isOnScreen(y, section) {
         let p = section.querySelector('.col-2');
         if (y >= section.offsetTop && y < section.clientHeight + section.offsetTop) {
-            /* section.style.setProperty("opacity", 1); */
-            /* console.log(section.children) */
             for (const item of section.children) {
                 item.style.setProperty("opacity", 1);   //se los pongo por separado al IMG y al P porque si se lo hago al SECTION
                 //no puedo agregarle transicion al P
